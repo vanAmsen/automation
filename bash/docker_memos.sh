070a822f399609d49a24060cb13a40122a398e39
Edgar <edgar.amsen@gmail.com>
2024-02-23 13:33:57 +0100

    Update and rename setup_and_run.sh to docker_memos.sh

--- bash/docker_memos.sh
+++ bash/docker_memos.sh
@@ -4,12 +4,15 @@
 sudo apt update
 sudo apt upgrade -y
 
+# Install Git
+sudo apt install git -y
+
 # Install Ansible
 sudo apt install ansible -y
 
 # Clone your Ansible playbook repository
-git clone https://github.com/yourusername/your-private-repo.git
-cd your-private-repo
+git clone https://github.com/vanAmsen/automation.git
+cd automation/ansible
 
 # Run the Ansible playbook
 ansible-playbook -i 'localhost,' docker_memos_install.yml -c local
